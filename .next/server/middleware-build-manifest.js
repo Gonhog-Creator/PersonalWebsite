b7f3606faee30aed8c6ba9ca0f872a5c147bdd1b
josemariabarbeito <josemaria.barbeito@icloud.com>
2025-08-29 19:11:14 -0400

    Update GitHub Actions workflow with better error handling

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-d07058fe7175d07b.js","static/chunks/4bd1b696-f785427dddbba9fb.js","static/chunks/1255-2fcae74b25cc8a74.js","static/chunks/main-app-cbd363707e7c2c34.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-d07058fe7175d07b.js","static/chunks/framework-4a99af1472046e21.js","static/chunks/main-1c9b14883abe82c3.js","static/chunks/pages/_app-6c8c2371b16a04b8.js"],"/_error":["static/chunks/webpack-d07058fe7175d07b.js","static/chunks/framework-4a99af1472046e21.js","static/chunks/main-1c9b14883abe82c3.js","static/chunks/pages/_error-94812ad32cad7365.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
